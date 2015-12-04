ea5b2d8a5d71b55bea3adc6b43ae464e6eecaa46
Spyke15 <stasspyke15@gmail.com>
2015-12-04 12:35:07 +0200

    my style

---
+++ script_bandits.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".calendar div").hover(function(){
+		$(".calendarHover").animate({height: 150}, 200, 'easeOutBounce');
+		},function(){
+		$(".calendarHover").animate({height: 0},0);   // эффект выпадения дополнительного блока календаря
+	})
+
+	$('.carousel').carousel({   // интервал переключение слайдов
+  	interval: 3500
+})
+})
