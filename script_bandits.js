130cabec6dc64ff8b069d56e734e0d15562e7946
Spyke15 <stasspyke15@gmail.com>
2015-12-12 12:33:25 +0200

    change calendar hover animation

--- script_bandits.js
+++ script_bandits.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".calendar div").hover(function(){
-		$(".calendarHover").animate({height: 150}, 200, 'easeOutBounce');
+		$(".calendarHover").animate({height: 150}, 150, 'easeInBack');
 		},function(){
 		$(".calendarHover").animate({height: 0},0);   // эффект выпадения дополнительного блока календаря
 	})
